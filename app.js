c1adfcbfa949e1a8af9566bb2f8369b78940e219
mcbobby <madumcbobby@yahoo.com>
2022-01-10 13:22:38 +0100

    Update app.js

--- app.js
+++ app.js
@@ -26,16 +26,19 @@ app.use(
   })
 );
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/build")));
-  app.get("*", (req, res) =>
-    res.sendFile(path.join(__dirname, "build/index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running....");
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "/build")));
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.join(__dirname, "build/index.html"))
+//   );
+// } else {
+//   app.get("/", (req, res) => {
+//     res.send("API is running....");
+//   });
+// }
+
+// app.use(notFound);
+// app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
